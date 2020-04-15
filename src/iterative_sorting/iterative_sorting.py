a594add226305fe26a8fc69b436d610331808ff5
Mr. Jonah <jaitch@gmail.com>
2020-04-14 19:47:50 -0500

    It lives!

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,7 +6,7 @@ def selection_sort(arr):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        for num in range(i + 1, len(arr)):
+        for num in range(cur_index, len(arr)):
             if arr[num] < arr[smallest_index]:
                 smallest_index = num
         # TO-DO: swap
@@ -14,7 +14,6 @@ def selection_sort(arr):
             arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
-
 
 # TO-DO:  implement the Bubble Sort function below
 
@@ -26,6 +25,18 @@ def bubble_sort(arr):
                 arr[num], arr[num + 1] = arr[num + 1], arr[num]
 
     return arr
+
+def bubble_sort_guided(arr):
+    swaps_occured = True
+    while swaps_occured:
+        swaps_occured = False
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swaps_occured = True
+    return arr
+
+print(bubble_sort_guided([5,2,8,7,3,4,6,1,9,0]))
 
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,8 +1,33 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+    # merged_arr = [0] * elements
+    merged_arr = []
     # TO-DO
+    pos = 0 # merged_arr postition
+    i = 0
+    j = 0
+
+    while len(arrA) != 0 and len(arrB) != 0:
+        if arrA[0] < arrB[0]:
+            merged_arr.append(arrA[0])
+            arrA.remove(arrA[0])
+        else:
+            merged_arr.append(arrB[0])
+            arrB.remove(arrB[0])
+    if len(arrA) == 0:
+        merged_arr += arrB
+    else:
+        merged_arr += arrA
+
+    # while i < len(arrA) and j < len(arrB):
+    #     if arrA[i] < arrB[j]:
+    #         merged_arr[pos] = arrA[i]
+    #         i = i + 1
+    #     else:
+    #         merged_arr[pos] = arrB[j]
+    #         j = j + 1
+    #     pos += 1 # merged_arr position
     
     return merged_arr
 
@@ -11,8 +36,19 @@ def merge( arrA, arrB ):
 def merge_sort( arr ):
     # TO-DO
 
-    return arr
+    if len(arr) <= 1:
+        return arr
+    
+    # Initial split        
+    lhs = arr[:len(arr) // 2]
+    rhs = arr[len(arr) // 2:]
 
+    left = merge_sort(lhs)
+    right = merge_sort(rhs)
+    
+    return merge(left, right)
+
+print(merge_sort([2,8,6,0,1,7,5,4,3,9]))
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
